0ae640222fa53116eb81a9799b0e24e949795887
Chris Frewin <frewin.christopher@gmail.com>
2020-05-27 11:20:06 +0200

    multiple site functionality

--- src/CronServer.ts
+++ src/CronServer.ts
@@ -2,15 +2,21 @@ import cron from 'node-cron';
 import env from "./env/.env.json";
 import { launchChromeAndProcessLighthouseResponse } from './utils/utils';
 
-// run lighthouse monitor once per day at 10:00
-if (env.NODE_ENV === "DEVELOP") {
-    console.log('Develop environment; running immediately');
-    launchChromeAndProcessLighthouseResponse('https://chrisfrew.in')
-} else {
-    cron.schedule(
-        '0 10 * * *',
-        () => launchChromeAndProcessLighthouseResponse('https://chrisfrew.in')
-    );
-}
+const sites = ['https://chrisfrew.in', 'https://xn--seelengeflster-tirol-yec.com/'];
+
+
+sites.forEach(site => {
+    if (env.NODE_ENV === "DEVELOP") {
+        console.log('Develop environment; running immediately');
+        launchChromeAndProcessLighthouseResponse(site)
+    } else {
+        // run lighthouse monitor once per day at 10:00
+        cron.schedule(
+            '0 10 * * *',
+            () => launchChromeAndProcessLighthouseResponse(site)
+        );
+    }
+})
+
 
 
